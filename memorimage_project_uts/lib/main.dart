4b32c045c742fc9fa986c114ccceff663630dfc7
stevennchristopher <stchrist03@gmail.com>
2024-04-12 20:47:21 +0700

    add route

--- memorimage_project_uts/lib/main.dart
+++ memorimage_project_uts/lib/main.dart
@@ -50,6 +50,9 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        
+      },
     );
   }
 }
